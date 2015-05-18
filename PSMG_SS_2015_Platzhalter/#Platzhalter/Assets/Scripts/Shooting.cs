496658620e42d8f4fecca0fa5196fec49f769398
Kinderfest <DominikDeller@gmx.de>
2015-05-18 11:50:43 +0200

    AddedWeaponChange

--- PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/Shooting.cs
+++ PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/Shooting.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class Shooting : MonoBehaviour {
 
-    private float speed = 25;
+    private float speed = 25f;
+    private float vertical = 0f;
+    private float damage = 10;
     public Rigidbody projectile;
 
 	// Use this for initialization
@@ -17,9 +19,21 @@ public class Shooting : MonoBehaviour {
         if (Input.GetButtonDown("Fire1"))
         {
             Rigidbody instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
-            instantiatedProjectile.velocity = transform.TransformDirection(new Vector2(speed, 0f));
+            instantiatedProjectile.velocity = transform.TransformDirection(new Vector2(speed, vertical));
 
             Destroy(instantiatedProjectile.gameObject, 1);
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (vertical == 10f)
+            {
+                vertical = 0f;
+            }
+            else
+            {
+                vertical = 10f;
+            }
         }
 	
 	}

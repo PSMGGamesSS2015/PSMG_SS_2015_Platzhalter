85b9443ad30172168080a1ba5622d272c9c61d5d
Owlwald <ewaldreinhardt@web.de>
2015-05-21 17:35:02 +0200

    Materials, 2nd Weapon, Cleanup
    Added Materials for ground and platforms, changed second weapon so it
    shoots two projectiles, as planned, and cleaned up the code a bit.

--- PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/Shooting.cs
+++ PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/Shooting.cs
@@ -8,13 +8,14 @@ public class Shooting : MonoBehaviour {
     private float vertical = 0f;
     private float damage = 10;
     public Rigidbody projectile;
-    private Rigidbody instantiatedProjectile;
+	private Rigidbody instantiatedProjectile, instantiatedProjectile2;
 	public AudioSource shootSound;
 	public Image Weapon_1,Weapon_2;
+	private int weapon;
 
 	// Use this for initialization
 	void Start () {
-	
+		weapon = 1;
 	}
 	
 	// Update is called once per frame
@@ -22,31 +23,61 @@ public class Shooting : MonoBehaviour {
 
         if (Input.GetButtonDown("Fire1"))
         {
-            instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
-            instantiatedProjectile.velocity = transform.TransformDirection(new Vector2(speed, vertical));
-			shootSound.Play ();
+			switch(weapon){
+			case 1:
+				fireWeaponOne();
+				break;
+			case 2:
+				fireWeaponTwo();
+				break;
+			}
 
-            Destroy(instantiatedProjectile.gameObject, 1);
+
         }
 
         if (Input.GetButtonDown("Fire2"))
         {
-            if (vertical == 10f)
+            if (weapon==2)
             {
-                vertical = 0f;
-				shootSound.pitch=1f;
-				Weapon_1.enabled=true;
-				Weapon_2.enabled=false;
+				switchToWeaponOne();
             }
-            else
+            else if(weapon==1)
             {
-                vertical = 10f;
-				shootSound.pitch=0.4f;
-				Weapon_1.enabled=false;
-				Weapon_2.enabled=true;
+				switchToWeaponTwo();
             }
         }
 	
+	}
+	private void fireWeaponOne(){
+		instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+		instantiatedProjectile.velocity = transform.TransformDirection(new Vector2(speed, vertical));
+		shootSound.Play ();
+
+		Destroy(instantiatedProjectile.gameObject, 1);
+	}
+	private void fireWeaponTwo(){
+		instantiatedProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+		instantiatedProjectile2 = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+		instantiatedProjectile.velocity = transform.TransformDirection(new Vector2(speed, vertical));
+		instantiatedProjectile2.velocity =transform.TransformDirection (new Vector2(speed,-vertical));
+		shootSound.Play ();
+	
+		Destroy(instantiatedProjectile.gameObject, 1);
+		Destroy(instantiatedProjectile2.gameObject,1);
+	}
+	private void switchToWeaponOne(){
+		weapon=1;
+		vertical = 0f;
+		shootSound.pitch=1f;
+		Weapon_1.enabled=true;
+		Weapon_2.enabled=false;
+	}
+	private void switchToWeaponTwo(){
+		weapon=2;
+		vertical = 9f;
+		shootSound.pitch=0.4f;
+		Weapon_1.enabled=false;
+		Weapon_2.enabled=true;
 	}
 
     public void setSpeed(int direction)

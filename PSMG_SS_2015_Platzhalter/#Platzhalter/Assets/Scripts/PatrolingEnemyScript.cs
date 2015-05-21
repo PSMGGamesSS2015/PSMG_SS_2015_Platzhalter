0a1c91e25443c2948fe39497c79718617ebb31d5
Kinderfest <DominikDeller@gmx.de>
2015-05-21 20:27:10 +0200

    MergedEnemies

---
+++ PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/PatrolingEnemyScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PatrolingEnemyScript : MonoBehaviour {
+	
+	public float moveSpeed;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.Translate (new Vector3 (moveSpeed, 0, 0) * Time.deltaTime);
+		
+	}
+	
+	void OnCollisionEnter2D(Collision2D col){
+		
+		if (col.gameObject.tag == "Box") {
+			
+			moveSpeed *= -1;
+			
+		}
+		
+		if (col.gameObject.tag == "Player") {
+			
+			Destroy(col.gameObject);
+			
+		}
+		
+	}
+	
+	
+}

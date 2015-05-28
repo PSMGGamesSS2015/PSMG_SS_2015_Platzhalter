5ebc11803b550f5d1842689e80f55ead3a6d6d07
webphil <weberphilipp@arcor.de>
2015-05-28 19:22:57 +0200

    Shooting Enemy

---
+++ PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/ShootingEnemy.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShootingEnemy : MonoBehaviour {
+
+	private float speed = -25f;
+	private float vertical = 0f;
+	private float damage = 10;
+	public GameObject projectile;
+	public AudioSource shootSound;
+	private int weapon;
+	private GameObject bullet;
+	private GameObject bullet_weapon2_1,bullet_weapon2_2;
+	private float distance;
+
+	public GameObject ui_controller;
+	
+
+	// Use this for initialization
+	void Start () {
+
+		InvokeRepeating("fire", 2, 2);
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+			
+	}
+
+	private void fire(){
+		bullet = Instantiate (projectile, transform.position, transform.rotation) as GameObject;
+		bullet.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector2(speed, vertical));
+		shootSound.Play ();
+		Destroy (bullet.gameObject, 1);
+	}
+
+}

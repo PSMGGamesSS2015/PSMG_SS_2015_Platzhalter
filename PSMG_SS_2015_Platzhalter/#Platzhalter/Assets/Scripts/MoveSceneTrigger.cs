fb8cf255a5fe59191985aea86b4f9d6048547b5c
Kinderfest <DominikDeller@gmx.de>
2015-05-31 21:14:12 +0200

    ChangeLevel

---
+++ PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/MoveSceneTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveSceneTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider newLevel)
+    {
+        Debug.Log("Hallo");
+        if (newLevel.gameObject.tag == "Player")
+        {
+            Application.LoadLevel("NewScene");
+        }
+    }
+}

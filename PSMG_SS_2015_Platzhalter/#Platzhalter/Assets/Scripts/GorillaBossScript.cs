b20298603450c23f48c1dceb313bb36365ccfd54
webphil <weberphilipp@arcor.de>
2015-06-09 18:00:52 +0200

    BossRoom and BossScene

---
+++ PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/GorillaBossScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GorillaBossScript : MonoBehaviour {
+
+	public Vector3 targetPosition;
+	public float moveSpeed;
+	
+	void Start()
+	{
+		StartCoroutine(MoveToPosition(targetPosition));
+	}
+	
+	IEnumerator MoveToPosition(Vector3 target)
+	{
+		while (transform.position != target)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, target, moveSpeed * Time.deltaTime);
+			yield return 0;
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

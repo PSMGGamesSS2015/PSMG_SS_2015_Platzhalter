14ede5f225f6e928cb564cf2adf944ecae8ae6ba
Kinderfest <DominikDeller@gmx.de>
2015-06-23 12:59:49 +0200

    Enemies up to date

---
+++ PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/Enemies/Level 1/Regular Enemies/ShootingEnemy.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShootingEnemy : MonoBehaviour {
+
+	private float speed = -25f;
+	private float vertical = 0f;
+    private float health = 30f;
+	public GameObject projectile;
+	private GameObject bullet;
+	private float distance= 0.7f;
+
+	
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("fire", 2, 2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        checkHealth();
+	}
+
+	private void fire(){
+		bullet = Instantiate (projectile, transform.position, transform.rotation) as GameObject;
+		bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(speed, vertical));
+		Destroy(bullet.gameObject, distance);
+	}
+
+    private void checkHealth()
+    {
+        if (health <= 0)
+        {
+            foreach (Transform childTransform in this.transform)
+            {
+                Destroy(childTransform.gameObject);
+            }
+            Destroy(this.gameObject);
+
+        }
+    }
+
+    private void onHit()
+    {
+        health -= 10; 
+    }
+
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.gameObject.tag == "BulletPlayer")
+        {
+            onHit();
+        }
+    }
+
+}
---
+++ PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/Player/SimplePlatformController.cs
@@ -0,0 +1,138 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SimplePlatformController : MonoBehaviour {
+
+    private bool facingRight = true;
+    private bool jump = false;
+
+    public float moveForce = 365f;
+    public float maxSpeed = 5f;
+    private float jumpForce = 500f;
+
+    public Transform groundCheck;
+
+    public GameObject player;
+    public GameObject UIController;
+
+    private bool grounded = true;
+    private Rigidbody2D rb2d;
+
+	public AudioSource jumpSound, deathSound;
+
+	private bool god;
+	private bool pause;
+
+
+	private int health;
+
+	// Use this for initialization
+	void Start() {
+		health = 100;
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            jump = true;
+        }
+
+		//Godmode shenanigans
+
+			if (Input.GetKey (KeyCode.G) && Input.GetKey (KeyCode.LeftShift)) {
+				god=true;
+				health=100000;
+				maxSpeed=25f;
+			}
+			if (Input.GetButtonDown ("Jump") && god) {
+				jump=true;
+			}
+
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        //anim.SetFloat("Speed", Mathf.Abs(h));
+
+        if (h * rb2d.velocity.x < maxSpeed)
+        {
+            rb2d.AddForce(Vector2.right * h * moveForce);
+        }
+
+        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+        {
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+        }
+
+        if (h > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (h < 0 && facingRight)
+        {
+            Flip();
+        }
+
+        if (jump)
+        {
+            //anim.SetTrigger("Jump");
+			//jumpSound.Play ();
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+            jump = false;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        GameObject player;
+        player = GameObject.Find("PlayerModel");
+        player.transform.RotateAround(transform.position, transform.up, 180f);
+    }
+	public void onHit(){
+		health -= 20;
+        UIController.GetComponent<UIScript>().update_life(health);
+
+        if (health <= 0)
+        {
+			deathSound.Play();
+
+			Destroy(this.gameObject);
+				Application.LoadLevel("Game Over");
+			
+        }
+	}
+
+
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.gameObject.tag == "DeathZone")
+        {
+			deathSound.Play ();
+
+				Application.LoadLevel("Game Over");
+			
+		}
+
+        if (player.gameObject.tag == "Goal")
+        {
+			Destroy (GameObject.Find("LevelSelector").gameObject);
+            Application.LoadLevel("Level 1 Boss");
+        }
+
+        if (player.gameObject.tag == "Enemy" || player.gameObject.tag == "BulletEnemy")
+        {
+            onHit();        
+        }
+    }
+	IEnumerator waitfordeath(){
+		yield return new WaitForSeconds(3);
+	}
+
+}

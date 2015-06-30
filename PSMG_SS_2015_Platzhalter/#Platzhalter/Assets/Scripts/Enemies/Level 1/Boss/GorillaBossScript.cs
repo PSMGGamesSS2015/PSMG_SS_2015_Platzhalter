01000f6cfdf89d99d2e65e9f45ca41397af96ccd
Owlwald <ewaldreinhardt@web.de>
2015-06-30 22:08:40 +0200

    Copied Stuff from enemies
    transferred the latest changes from the enemies branch

--- PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/Enemies/Level 1/Boss/GorillaBossScript.cs
+++ PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/Enemies/Level 1/Boss/GorillaBossScript.cs
@@ -17,13 +17,14 @@ public class GorillaBossScript : MonoBehaviour {
 	//Bullet
 	public GameObject projectileStraight,projectileCurve;
 	private GameObject bullet;
-	private float distance= 1f;
+	private float distance= 1.3f;
 	private float speed = -25f;
 	private float vertical = 0f;
 
-	private float health = 500;
+	private float health = 800;
 
 	private float rotation=180;
+	private float rotation2=90;
 	private float zposition=-13;
 
 	void Start()
@@ -83,16 +84,16 @@ public class GorillaBossScript : MonoBehaviour {
 				
 	}
 
-	/*private void moveMiddle(){
-		
-		StartCoroutine(moveMiddleAndWait());
-				
-	}*/
-
 	private void groundStomp(){
 		
 		StartCoroutine (moveMiddleTopAndWait ());
 		
+	}
+
+	private void changePosAfterGroundStomp() {
+
+		StartCoroutine (moveRightFromMiddleTop ());
+
 	}
 
 	private void moveRight(){
@@ -119,17 +120,29 @@ public class GorillaBossScript : MonoBehaviour {
 		bullet = Instantiate (projectileCurve, pos, transform.rotation) as GameObject;
 		bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(Random.Range(-10,-12), 18));
 		Destroy(bullet.gameObject, 3f);
+		bullet = Instantiate (projectileCurve, pos, transform.rotation) as GameObject;
+		bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(Random.Range(-12,-14), 20));
+		Destroy(bullet.gameObject, 3f);
+		bullet = Instantiate (projectileCurve, pos, transform.rotation) as GameObject;
+		bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(Random.Range(-14,-16), 22));
+		Destroy(bullet.gameObject, 3f);
 	}
 	private void fireArcRight(){
 		Vector3 pos = new Vector3 (transform.position.x - 2, transform.position.y, transform.position.z);
 		bullet = Instantiate (projectileCurve, pos, transform.rotation) as GameObject;
-		bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(Random.Range(-4,-8), 12));
+		bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(Random.Range(-4,-8), 14));
 		Destroy(bullet.gameObject, 3f);
 		bullet = Instantiate (projectileCurve, pos, transform.rotation) as GameObject;
-		bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(Random.Range(-6,-10), 15));
+		bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(Random.Range(-6,-10), 16));
 		Destroy(bullet.gameObject, 3f);
 		bullet = Instantiate (projectileCurve, pos, transform.rotation) as GameObject;
 		bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(Random.Range(-10,-12), 18));
+		Destroy(bullet.gameObject, 3f);
+		bullet = Instantiate (projectileCurve, pos, transform.rotation) as GameObject;
+		bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(Random.Range(-12,-14), 20));
+		Destroy(bullet.gameObject, 3f);
+		bullet = Instantiate (projectileCurve, pos, transform.rotation) as GameObject;
+		bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(Random.Range(-14,-16), 22));
 		Destroy(bullet.gameObject, 3f);
 	}
 
@@ -203,13 +216,12 @@ public class GorillaBossScript : MonoBehaviour {
 		var pointC = targetLeftPosition;
 		var pointB = targetMiddleTopPosition;
 
-		Vector3 point = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
-
 		yield return StartCoroutine(MoveObject(transform, pointA, pointB, 0.5f));
 		yield return StartCoroutine (MoveObject (transform, pointB, pointC, 0.5f));
 
+		Vector3 point = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
 		transform.RotateAround (point, Vector3.up, rotation);
-		transform.position = new Vector3 (112, 3, -9);
+		//transform.position = new Vector3 (112, 3, -9);
 
 		yield return new WaitForSeconds(1.2f);
 		leftStraight ();
@@ -219,33 +231,19 @@ public class GorillaBossScript : MonoBehaviour {
 	IEnumerator moveRightAndWait(){
 		
 		var pointA = transform.position;
-		var pointB/*C*/ = targetRightPosition;
+		var pointB = targetRightPosition;
 		//var pointB = targetMiddleTopPosition;
-
-		Vector3 point = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
 		
 		yield return StartCoroutine(MoveObject(transform, pointA, pointB, 0.5f));
 		//yield return StartCoroutine (MoveObject (transform, pointB, pointC, 0.5f));
-
+		Vector3 point = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
 		transform.RotateAround (point, Vector3.up, rotation);
-		transform.position = new Vector3 (128, 3, -8);
+		//transform.position = new Vector3 (128, 3, -8);
 
 		yield return new WaitForSeconds(1.2f);
 		rightStraight ();
 		
 	}
-
-	/*IEnumerator moveMiddleAndWait(){
-		
-		var pointA = transform.position;
-		var pointB = targetMiddlePosition;
-		
-		yield return StartCoroutine(MoveObject(transform, pointA, pointB, 0.5f));
-		
-		yield return new WaitForSeconds(1.2f);
-		groundStomp ();
-		
-	}*/
 
 	IEnumerator moveMiddleTopAndWait(){
 		
@@ -257,11 +255,47 @@ public class GorillaBossScript : MonoBehaviour {
 		yield return new WaitForSeconds(0.5f);
 		pointA = transform.position;
 		pointB = targetMiddlePosition;
+
+		Vector3 point = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+		//GameObject.Find ("gorilla 2").transform.RotateAround (point, Vector3.up, rotation2);
+
+		yield return StartCoroutine(MoveObject(transform, pointA, pointB, 0.5f));
+
+		Vector3 pos = new Vector3 (transform.position.x, transform.position.y-1.5f, transform.position.z);
+		bullet = Instantiate (projectileStraight,pos , transform.rotation) as GameObject;
+		bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(speed, vertical));
+		Destroy(bullet.gameObject, distance);
+		
+		Vector3 pos2 = new Vector3 (transform.position.x, transform.position.y-1.5f, transform.position.z);
+		bullet = Instantiate (projectileStraight,pos , transform.rotation) as GameObject;
+		bullet.transform.RotateAround(transform.position, transform.up, 180f);
+		bullet.GetComponent<Rigidbody2D>().velocity = transform.TransformDirection(new Vector2(-speed, vertical));
+		Destroy(bullet.gameObject, distance);
+		//GameObject.Find ("gorilla 2").transform.RotateAround (point, Vector3.up, -rotation2);
+
+		yield return new WaitForSeconds(1.2f);
+
+		changePosAfterGroundStomp ();
+		
+	}
+
+	IEnumerator moveRightFromMiddleTop(){
+
+		var pointA = transform.position;
+		var pointB = targetMiddleTopPosition;
 		
 		yield return StartCoroutine(MoveObject(transform, pointA, pointB, 0.5f));
 
+		pointA = transform.position;
+		pointB = targetRightPosition;
+
+		yield return StartCoroutine(MoveObject(transform, pointA, pointB, 0.5f));
+				
+		Vector3 point = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+		transform.RotateAround (point, Vector3.up, rotation);
+
 		yield return new WaitForSeconds(1.2f);
-		moveRight ();
+		rightStraight ();
 		
 	}
 

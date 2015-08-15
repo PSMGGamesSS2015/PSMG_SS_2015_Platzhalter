64c92dc26fad39871d56944b36371d9415c73d4a
Owlwald <ewaldreinhardt@web.de>
2015-08-15 12:39:30 +0200

    Level 4 Obstacles

---
+++ PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/fireBallScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireBallScript : MonoBehaviour {
+	public Transform pos1;
+	public Vector3 pointB;
+	void Start () {
+		pointB = pos1.position;
+	}
+
+	void Update () {
+		StartCoroutine (Moving ());
+	}
+
+	IEnumerator Moving(){
+		Vector3 pointA = transform.position;
+		while (true) {
+			yield return StartCoroutine (MoveObject (transform, pointA,pointB, 2f));
+			yield return StartCoroutine (MoveObject (transform, pointB,pointA, 2f));
+		}
+	}
+
+	IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+	{
+		var i= 0.0f;
+		var rate= 1.0f/time;
+		while (i < 1.0f) {
+			i += Time.deltaTime * rate;
+			thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+			yield return null; 
+		}
+	}
+}
---
+++ PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/rotatingFireScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotatingFireScript : MonoBehaviour {
+	public Transform rotateAroundPoint;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(rotateAroundPoint.position, Vector3.forward, 75 * Time.deltaTime);
+	}
+}

b60cbd219047d419fc3de40d2e0de17ad9608ed5
Owlwald <ewaldreinhardt@web.de>
2015-08-15 13:20:41 +0200

    Walljump copied from movement branch

--- PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/Player/SimplePlatformController.cs
+++ PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/Player/SimplePlatformController.cs
@@ -4,57 +4,61 @@ using UnityEngine.UI;
 using XInputDotNetPure;
 
 public class SimplePlatformController : MonoBehaviour {
-
-    private bool facingRight = true;
-    public bool jump = false;
+	
+	private bool facingRight = true;
+	public bool jump = false;
+	public bool wallJump = false;
+	private bool wallJumping = true;
 	public Animator anim;
 	private bool invincible = false;
-    public float moveForce = 365f;
-    public float maxSpeed = 5f;
-    private float jumpForce = 500f;
-
+	public float moveForce = 365f;
+	public float maxSpeed = 5f;
+	private float jumpForce = 500f;
+	
 	public float climbSpeed = 5f;
 	private float climbVelocity;
 	private float gravityStore;
-
-    public Transform groundCheck;
-
-    public GameObject player;
-    public GameObject UIController;
-
-    public bool grounded = true;
-    private Rigidbody2D rb2d;
-
+	
+	public Transform groundCheck;
+	public Transform wallCheck;
+	
+	public GameObject player;
+	public GameObject UIController;
+	
+	public bool grounded = true;
+	public bool walled = true;
+	private Rigidbody2D rb2d;
+	
 	private Vector2 boostSpeed = new Vector2(700,0);
 	private float boostCooldown = 0.1f;
 	private bool canBoost = true;
-
+	
 	public AudioSource jumpSound, deathSound, lifeSound;
-
+	
 	private bool god;
 	private bool pause;
 	public bool onLadder;
-
+	
 	private GameObject lvlCheck;
 	private GameObject lifes;
-
+	
 	private int health;
 	
-
+	
 	// Use this for initialization
 	void Start() {
-
+		
 		health = 100;
-        rb2d = GetComponent<Rigidbody2D>();
+		rb2d = GetComponent<Rigidbody2D>();
 		lvlCheck = GameObject.Find ("_GM");
 		lifes = GameObject.Find ("PlayerLifes");
 		gravityStore = rb2d.gravityScale;
-
-    }
+		
+	}
 	
 	// Update is called once per frame
-    void Update()
-    {
+	void Update()
+	{
 		if(Input.GetKeyDown (KeyCode.F1)){
 			Application.LoadLevel("Level 1");
 		}
@@ -64,56 +68,64 @@ public class SimplePlatformController : MonoBehaviour {
 		if(Input.GetKeyDown (KeyCode.F3)){
 			Application.LoadLevel("Level 2");
 		}
-
+		
 		UIController.GetComponent<UIScript> ().update_lifes (lifes.GetComponent<LifeScript>().lifes);
-        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
-        if ((Input.GetButtonDown("Jump") && grounded )|| (Input.GetButtonDown("Jump")&&onLadder))
-        {
-            jump = true;
+		
+		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+		walled = Physics2D.Linecast(transform.position, wallCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+		
+		if ((Input.GetButtonDown("Jump") && grounded )|| (Input.GetButtonDown("Jump")&&onLadder))
+		{
+			jump = true;
 			anim.SetBool("Jump",true);
 			jumpSound.Play ();
-        }
+		}
+		if(Input.GetButtonDown("Jump")&&walled){
+			wallJump = true;
+			anim.SetBool("Jump",true);
+			jumpSound.Play ();
+		}
 		if (grounded && canBoost && Input.GetButtonDown("Fire3")&&lvlCheck.GetComponent<LevelCheck>().levelOneDone==true) {
 			StartCoroutine(Boost(0.3f));
 		}
-
+		
 		//Godmode shenanigans
-
-			if (Input.GetKey (KeyCode.G) && Input.GetKey (KeyCode.LeftShift)) {
-				god=true;
-				health=100000;
-				maxSpeed=15f;
-			}
-			if (Input.GetButtonDown ("Jump") && god) {
-				jump=true;
-			}
-    }
-
-    void FixedUpdate()
-    {
-        float h = Input.GetAxis("Horizontal");
+		
+		if (Input.GetKey (KeyCode.G) && Input.GetKey (KeyCode.LeftShift)) {
+			god=true;
+			health=100000;
+			maxSpeed=15f;
+		}
+		if (Input.GetButtonDown ("Jump") && god) {
+			jump=true;
+		}
+	}
+	
+	void FixedUpdate()
+	{
+		float h = Input.GetAxis("Horizontal");
 		float v = Input.GetAxis("Vertical");
-        anim.SetFloat("Walking", Mathf.Abs(h));
-
-        if (h * rb2d.velocity.x < maxSpeed)
-        {
-            rb2d.AddForce(Vector2.right * h * moveForce);
-        }
-
-        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
-        {
-            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
-        }
-
+		anim.SetFloat("Walking", Mathf.Abs(h));
+		
+		if (h * rb2d.velocity.x < maxSpeed)
+		{
+			rb2d.AddForce(Vector2.right * h * moveForce);
+		}
+		
+		if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+		{
+			rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+		}
+		
 		if (onLadder) {
 			rb2d.gravityScale = 0f;
 			rb2d.velocity = new Vector2(0,0);
-
+			
 			if (v * rb2d.velocity.y < maxSpeed)
 			{
 				rb2d.AddForce(Vector2.up * v * moveForce);
 			}
-
+			
 			if(jump){
 				StartCoroutine(jumpFromLadder());
 			}
@@ -121,24 +133,36 @@ public class SimplePlatformController : MonoBehaviour {
 		if (!onLadder) {
 			rb2d.gravityScale = gravityStore;
 		}
-        if (h > 0 && !facingRight)
-        {
-            Flip();
-        }
-        else if (h < 0 && facingRight)
-        {
-            Flip();
-        }
-        if (jump)
-        {
-            rb2d.AddForce(new Vector2(0f, jumpForce));
-            jump = false;
+		if (h > 0 && !facingRight)
+		{
+			Flip();
+		}
+		else if (h < 0 && facingRight)
+		{
+			Flip();
+		}
+		if (jump)
+		{
+			rb2d.AddForce(new Vector2(0f, jumpForce));
+			jump = false;
 			anim.SetBool("Jump",false);
-        }
+		}
+		if (wallJump&&lvlCheck.GetComponent<LevelCheck>().levelThreeDone==true) {
+			if(wallJumping){
+				rb2d.AddForce(new Vector2(0f, jumpForce));
+				if (facingRight) {
+					rb2d.AddForce (new Vector2 (-5f, 3f), ForceMode2D.Impulse);
+				}
+				else rb2d.AddForce (new Vector2 (5f, 3f), ForceMode2D.Impulse);
+				wallJump = false;
+				anim.SetBool("Jump",false);
+				StartCoroutine(wallJumpingCooldown());
+			}
+		}
 		if (Input.GetButtonUp ("Horizontal")) {
 			anim.SetFloat("Walking",0f);
 		}
-    }
+	}
 	IEnumerator jumpFromLadder(){
 		onLadder=false;
 		yield return new WaitForSeconds (1.0f);
@@ -162,16 +186,16 @@ public class SimplePlatformController : MonoBehaviour {
 		yield return new WaitForSeconds (boostCooldown);
 		canBoost = true;
 	}
-    void Flip()
-    {
-        facingRight = !facingRight;
-        GameObject player;
-        player = GameObject.Find("PlayerModel");
-        player.transform.RotateAround(transform.position, transform.up, 180f);
-    }
+	void Flip()
+	{
+		facingRight = !facingRight;
+		GameObject player;
+		player = GameObject.Find("PlayerModel");
+		player.transform.RotateAround(transform.position, transform.up, 180f);
+	}
 	public void onHit(){
 		health -= 20;
-        UIController.GetComponent<UIScript>().update_life(health);
+		UIController.GetComponent<UIScript>().update_life(health);
 		StartCoroutine (ControllerRumble ());
 		/*if (facingRight) {
 			rb2d.AddForce (new Vector2 (-5f, 3f), ForceMode2D.Impulse);
@@ -180,10 +204,10 @@ public class SimplePlatformController : MonoBehaviour {
 		*/
 		StartCoroutine (Blink ());
 		if (health <= 0)
-        {
+		{
 			//deathSound.Play();
 			onDeath();
-        }
+		}
 	}
 	void onDeath(){
 		Destroy(this.gameObject);
@@ -212,29 +236,34 @@ public class SimplePlatformController : MonoBehaviour {
 		lifeSound.Play ();
 		UIController.GetComponent<UIScript> ().update_life (health);
 	}
-
-    void OnTriggerEnter2D(Collider2D player)
-    {
-        if (player.gameObject.tag == "DeathZone")
-        {
+	
+	void OnTriggerEnter2D(Collider2D player)
+	{
+		if (player.gameObject.tag == "DeathZone")
+		{
 			onDeath ();
 		}
-        if (player.gameObject.tag == "Enemy" || player.gameObject.tag == "BulletEnemy")
-        {
+		if (player.gameObject.tag == "Enemy" || player.gameObject.tag == "BulletEnemy")
+		{
 			if (!invincible)
 			{
-            	onHit();        
+				onHit();        
 				StartCoroutine(invincibility());
 			}
 		}
 		if (player.gameObject.tag == "HealthUp") {
 			onHeal();
 		}
-    }
+	}
 	IEnumerator invincibility(){
 		invincible=true;
 		yield return new WaitForSeconds (0.6f);
 		invincible=false;
+	}
+	IEnumerator wallJumpingCooldown(){
+		wallJumping = false;
+		yield return new WaitForSeconds (1.6f);
+		wallJumping=true;
 	}
 	IEnumerator waitforfade(float fadeTime){
 		yield return new WaitForSeconds(fadeTime);

fd92e4b8d23cfa940606ab94a4a4983de62d72f4
Kinderfest <DominikDeller@gmx.de>
2015-08-16 15:19:35 +0200

    Added Pathfinding in Level 4

---
+++ PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/Enemies/Level 4/ChargingEnemy.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChargingEnemy : MonoBehaviour {
+
+	private float health = 30;
+	
+	private GameObject item;
+	private GameObject healthUp;
+	
+	// Use this for initialization
+	void Start () {
+		healthUp = GameObject.Find ("HealthUp");
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+		checkHealth();
+		}
+	
+	private void checkHealth()
+	{
+		if (health <= 0)
+		{
+			int i = Random.Range(1, 5);
+			if (i == 1)
+			{
+				item = Instantiate(healthUp, transform.position,  Quaternion.identity) as GameObject;
+			}
+			
+			foreach (Transform childTransform in this.transform)
+			{
+				Destroy(childTransform.gameObject);
+			}
+			Destroy(this.gameObject);
+			
+		}
+	}
+	
+	private void onHit(int i){
+		if (i == 1) {
+			health -= 10;
+		}
+		if (i == 2) {
+			health -= 20;
+		}
+		StartCoroutine (Blink ());
+	}
+	IEnumerator Blink(){
+		foreach (Transform child in transform) {                                                                                                                                                             
+			child.gameObject.GetComponent<Renderer> ().enabled = false;
+			yield return new WaitForSeconds (0.02f);
+			child.gameObject.GetComponent<Renderer> ().enabled = true;
+			yield return new WaitForSeconds (0.02f);
+		}
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.gameObject.tag == "BulletPlayer")
+		{
+			onHit(1);
+		}
+		if (collider.gameObject.tag == "Mine")
+		{
+			onHit(2);
+		}
+	}
+	
+}
+
---
+++ PSMG_SS_2015_Platzhalter/#Platzhalter/Assets/Scripts/Enemies/Level 4/EnemyAI.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (Seeker))]
+public class EnemyAI : MonoBehaviour {
+
+	public Transform target;
+
+	public float updateRate = 2f;
+	private Seeker seeker;
+	private Rigidbody2D rb2d;
+
+	public Path path;
+
+	private float speed = 1000f;
+	public ForceMode2D fMode;
+
+	[HideInInspector]
+	public bool pathIsEnded = false;
+
+	public float nextWaypointDistance = 3;
+	private int currentWaypoint = 0;
+
+	void Start()
+	{
+		seeker = GetComponent<Seeker> ();
+		rb2d = GetComponent<Rigidbody2D> ();
+
+		if (target == null) {
+			Debug.LogError("No Player found.");
+			return;
+		}
+
+		seeker.StartPath (transform.position, target.position, onPathComplete);
+
+		StartCoroutine (UpdatePath());
+	}
+
+	IEnumerator UpdatePath(){
+		if (target == null) {
+			return false;
+		}
+		seeker.StartPath (transform.position, target.position, onPathComplete);
+		yield return new WaitForSeconds (1f / updateRate);
+		StartCoroutine (UpdatePath ());
+	}
+
+	public void onPathComplete(Path p){
+		Debug.Log ("We got a path:" + p.error);
+		if (!p.error) {
+			path = p;
+			currentWaypoint = 0;
+		}
+	}
+
+	void FixedUpdate(){
+		if (target == null) {
+			return;
+		}
+		if (path == null) {
+			return;
+		}
+		if (currentWaypoint >= path.vectorPath.Count) {
+			if(pathIsEnded)
+				return;
+			Debug.Log ("End of path reached.");
+			pathIsEnded = true;
+			return;
+		}
+		pathIsEnded = false;
+		Vector3 dir = (path.vectorPath [currentWaypoint] - transform.position).normalized;
+		dir *= speed * Time.fixedDeltaTime;
+
+		rb2d.AddForce (dir,fMode);
+		float dist = Vector3.Distance (transform.position, path.vectorPath [currentWaypoint]);
+		if (dist < nextWaypointDistance) {
+			currentWaypoint++;
+			return;
+		}
+	}
+
+}
